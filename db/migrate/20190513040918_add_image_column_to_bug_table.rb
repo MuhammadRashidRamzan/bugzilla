d46078ecc80688c48789dd9368405f130aab894f
Dev <rashid@fliclicks.com>
2019-05-15 12:17:52 +0500

    change migration file

--- db/migrate/20190513040918_add_image_column_to_bug_table.rb
+++ db/migrate/20190513040918_add_image_column_to_bug_table.rb
@@ -1,9 +1,9 @@
 class AddImageColumnToBugTable < ActiveRecord::Migration
-   def up
-    add_attachment :bugs, :image
-  end
+  #  def up
+  #   add_attachment :bugs, :image
+  # end
 
-  def down
-    remove_attachment :bugs, :image
-  end
+  # def down
+  #   remove_attachment :bugs, :image
+  # end
 end
