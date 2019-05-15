64aaa6a05c6b31801a1a625577883781b9b00e3c
Dev <rashid@fliclicks.com>
2019-05-15 14:11:33 +0500

    change

--- db/migrate/20190507074352_create_projects.rb
+++ db/migrate/20190507074352_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string   :title
-      t.text   :description
+      t.text   :descrption
       t.timestamps null: false
     end
   end
