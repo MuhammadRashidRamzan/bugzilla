c5c181de3a778b1061c418d12672f19b9bd86b0f
Dev <rashid@fliclicks.com>
2019-05-15 12:41:10 +0500

    change migration

--- db/migrate/20190513040918_add_image_column_to_bug_table.rb
+++ db/migrate/20190513040918_add_image_column_to_bug_table.rb
@@ -3,7 +3,7 @@ class AddImageColumnToBugTable < ActiveRecord::Migration
   #   add_attachment :bugs, :image
   # end
 
-  # def down
-  #   remove_attachment :bugs, :image
-  # end
+  def down
+    remove_attachment :bugs, :image
+  end
 end
--- db/migrate/20190513050032_add_attachment_image_to_bugs.rb
+++ db/migrate/20190513050032_add_attachment_image_to_bugs.rb
@@ -1,5 +1,6 @@
 class AddAttachmentImageToBugs < ActiveRecord::Migration
   def self.up
+  	 remove_attachment :bugs, :image
     change_table :bugs do |t|
       t.attachment :image
     end
